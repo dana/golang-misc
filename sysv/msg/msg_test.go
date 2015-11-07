edd740b68f39d4ac726140ccef51860a64d45b3f
Dana M. Diederich <diederich@gmail.com>
2015-11-07 14:40:22 -0800

    wip

--- sysv/msg/msg_test.go
+++ sysv/msg/msg_test.go
@@ -1,7 +1,7 @@
 package sysvipc
 
 import (
-	"syscall"
+	//"syscall"
 	"testing"
 	"fmt"
 )
@@ -18,7 +18,9 @@ func TestSendRcv(t *testing.T) {
 	//		t.Error("msgrcv with bad msqid should fail", err)
 	//	}
 
+	fmt.Println("Before Send");
 	q.Send(7, []byte("test message body"), nil)
+	fmt.Println("Before Receive");
 	msg, mtyp, err := q.Receive(64, -10, nil)
 	if err != nil {
 		t.Error(err)
@@ -29,64 +31,66 @@ func TestSendRcv(t *testing.T) {
 	}
 }
 
-func TestNonBlockingSend(t *testing.T) {
-	msgSetup(t)
-	defer msgTeardown(t)
-
-	info, err := q.Stat()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = q.Set(&MQInfo{
-		Perms: IpcPerms{
-			OwnerUID: info.Perms.OwnerUID,
-			OwnerGID: info.Perms.OwnerGID,
-			Mode:     info.Perms.Mode,
-		},
-		MaxBytes: 8,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := q.Send(3, []byte("more than 8"), &MQSendFlags{DontWait: true}); err != syscall.EAGAIN {
-		t.Error("too-long write should have failed", err)
-	}
-}
-
-func TestNonBlockingReceive(t *testing.T) {
-	msgSetup(t)
-	defer msgTeardown(t)
-
-	_, _, err := q.Receive(64, -99, &MQRecvFlags{DontWait: true})
-	if err != syscall.EAGAIN && err != syscall.ENOMSG {
-		t.Error("non-blocking read against empty queue should fail", err)
-	}
-}
-
+//func TestNonBlockingSend(t *testing.T) {
+//	msgSetup(t)
+//	defer msgTeardown(t)
+//
+//	info, err := q.Stat()
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	err = q.Set(&MQInfo{
+//		Perms: IpcPerms{
+//			OwnerUID: info.Perms.OwnerUID,
+//			OwnerGID: info.Perms.OwnerGID,
+//			Mode:     info.Perms.Mode,
+//		},
+//		MaxBytes: 8,
+//	})
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	if err := q.Send(3, []byte("more than 8"), &MQSendFlags{DontWait: true}); err != syscall.EAGAIN {
+//		t.Error("too-long write should have failed", err)
+//	}
+//}
+//
+//func TestNonBlockingReceive(t *testing.T) {
+//	msgSetup(t)
+//	defer msgTeardown(t)
+//
+//	_, _, err := q.Receive(64, -99, &MQRecvFlags{DontWait: true})
+//	if err != syscall.EAGAIN && err != syscall.ENOMSG {
+//		t.Error("non-blocking read against empty queue should fail", err)
+//	}
+//}
+//
 var q MessageQueue
 
 func msgSetup(t *testing.T) {
-	//foo, _ := Ftok("/tmp",10)
-	//fmt.Println(foo)
+	fmt.Println("one")
 //	mq, err := GetMsgQueue(0xDA7ABA5E, &MQFlags{
 	mq, err := GetMsgQueue(17039435, &MQFlags{
 		Create:    true,
-//		Exclusive: true,
-		Exclusive: false,
+//		Create:    false,
+		Exclusive: true,
+//		Exclusive: false,
 		Perms:     0600,
 	})
+	fmt.Println("two");
 	if err != nil {
 		t.Fatal(err)
 	}
 //	mq := MessageQueue(17039435)
-	fmt.Println(mq)
+//	fmt.Println(mq)
 	q = mq
 }
 
 func msgTeardown(t *testing.T) {
-	return
+//	return
+//	fmt.Println(t)
 	if err := q.Remove(); err != nil {
 		t.Fatal(err)
 	}
