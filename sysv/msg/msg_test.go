69c925f96162d52f3a30c5ea8673bdfe09aaed0f
Dana M. Diederich <diederich@gmail.com>
2015-11-07 14:52:27 -0800

    wip

--- sysv/msg/msg_test.go
+++ sysv/msg/msg_test.go
@@ -8,29 +8,25 @@ import (
 
 func TestSendRcv(t *testing.T) {
 	msgSetup(t)
-	defer msgTeardown(t)
-
-	//	if err := q.Send(-1, nil, nil); err != syscall.EINVAL {
-	//		t.Error("msgsnd with negative mtyp should fail", err)
-	//	}
-
-	//	if _, _, err := MessageQueue(5).Receive(64, -1, nil); err != syscall.EINVAL {
-	//		t.Error("msgrcv with bad msqid should fail", err)
-	//	}
+//	defer msgTeardown(t)
 
 	fmt.Println("Before Send");
-	q.Send(7, []byte("test message body"), nil)
+//	q.Send(1, []byte("test message body"), nil)
+	RawSend([]byte("test message body"), q)
 	fmt.Println("Before Receive");
 	msg, mtyp, err := q.Receive(64, -10, nil)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if string(msg) != "test message body" || mtyp != 7 {
+	if string(msg) != "test message body" || mtyp != 1 {
 		t.Errorf("%q %v", string(msg), mtyp)
 	}
 }
 
+func RawSend(data []byte, queue MessageQueue) {
+	queue.Send(1, data, nil)
+}
 //func TestNonBlockingSend(t *testing.T) {
 //	msgSetup(t)
 //	defer msgTeardown(t)
@@ -83,15 +79,13 @@ func msgSetup(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-//	mq := MessageQueue(17039435)
-//	fmt.Println(mq)
 	q = mq
 }
 
-func msgTeardown(t *testing.T) {
+//func msgTeardown(t *testing.T) {
 //	return
 //	fmt.Println(t)
-	if err := q.Remove(); err != nil {
-		t.Fatal(err)
-	}
-}
+//	if err := q.Remove(); err != nil {
+//		t.Fatal(err)
+//	}
+//}
