db08a8993ae7dc781808cd93934f0bfe957c238f
Dana M. Diederich <diederich@gmail.com>
2015-11-08 13:21:08 -0800

    wip

--- sysv/parsing/parsing.go
+++ sysv/parsing/parsing.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-var test_input = []byte("11:d=localhost{\".ipc_transit_meta\":{\"destination\":\"localhost\",\"ttl\":9,\"destination_qname\":\"test\",\"send_ts\":1447014248},\"1\":2}")
+var testInput = []byte("19:d=localhost,foo=bar{\".ipc_transit_meta\":{\"destination\":\"localhost\",\"ttl\":9,\"destination_qname\":\"test\",\"send_ts\":1447014248},\"1\":2}")
 
 func TestParse(t *testing.T) {
 	if false {
@@ -17,18 +17,19 @@ func TestParse(t *testing.T) {
 }
 
 func main() {
-	parseWireHeader(test_input)
+	wireHeader, _ := parseWireHeader(testInput)
+	fmt.Println(wireHeader)
 }
 
-func parseWireHeader(test_input []byte) (map[string]string, error) {
+func parseWireHeader(testInput []byte) (map[string]string, error) {
 	var ret = make(map[string]string)
-	test_string := string(test_input)
-	parts := strings.SplitN(test_string, ":", 2)
-	header_length,atoiErr := strconv.Atoi(parts[0])
+	testString := string(testInput)
+	headerParts := strings.SplitN(testString, ":", 2)
+	headerLength,atoiErr := strconv.Atoi(headerParts[0])
 	if atoiErr != nil {
 		return ret, atoiErr
 	}
-	header_string := parts[1][0:header_length]
-	fmt.Println(header_string)
+	headerString := headerParts[1][0:headerLength]
+	fmt.Println(headerString)
 	return ret, nil
 }
